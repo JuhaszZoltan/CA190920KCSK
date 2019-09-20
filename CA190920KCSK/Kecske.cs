32ba56ca468a2746140c292b0255cccf4a45b011
Juhasz Zoltan <juhaszzoltanmate@yahoo.com>
2019-09-20 12:51:40 +0200

    Add project files.

---
+++ CA190920KCSK/Kecske.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA190920KCSK
+{
+    class Kecske
+    {
+        public string Nev { get; set; }
+        public bool Nem { get; set; }
+        public DateTime Szul { get; set; }
+        public Kecske(string nev, bool nem, DateTime szul)
+        {
+            Nev = nev;
+            Nem = nem;
+            Szul = szul;
+        }
+    }
+}
---
+++ CA190920KCSK/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA190920KCSK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kecskek = new List<Kecske>();
+
+            kecskek.Add(new Kecske("Géza", true, new DateTime(1997, 10, 01)));
+            kecskek.Add(new Kecske("Lukrécia", false, new DateTime(1992, 09, 17)));
+            kecskek.Add(new Kecske("János", true, new DateTime(1995, 07, 30)));
+            kecskek.Add(new Kecske("Gréta", false, new DateTime(1996, 01, 10)));
+
+            Console.WriteLine("Összes kecske neve: ");
+            foreach (var k in kecskek)
+            {
+                Console.WriteLine($"{k.Nev}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
